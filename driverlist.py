ef36a56652b0e90aea70bec382675368b7a86d4e
avjui <renej@vol.at>
2014-07-27 10:57:33 +0200

    Make driverslist resizeable
    # add boxsizer
    # change resolution of dialog
    # change layout ( buttons come to the left side

--- driverlist.py
+++ driverlist.py
@@ -11,7 +11,7 @@ class DriverList(wx.Dialog):
      
         def __init__(self, parent):
 
-                wx.Dialog.__init__(self, parent, -1, "Fahrerliste", size=(900,400),
+                wx.Dialog.__init__(self, parent, -1, "Fahrerliste", size=(900,473),
                                 style=wx.DEFAULT_DIALOG_STYLE|wx.THICK_FRAME|wx.RESIZE_BORDER|
                                 wx.TAB_TRAVERSAL)
 
@@ -20,27 +20,37 @@ class DriverList(wx.Dialog):
                 
         def __CreateDriversList(self):
                 
+                hbox = wx.BoxSizer(wx.HORIZONTAL)
+                inbox = wx.BoxSizer(wx.VERTICAL)
+
+                buttonbox1 = wx.BoxSizer(wx.HORIZONTAL)
+
+                hbox.Add((25,-1))
                 self.DropDownList = Database().GetAllData('Klassen', convert=True)                
-                wx.StaticText(self, -1, 'Nr.', pos=(20,20))
-                self.nbr = wx.TextCtrl(self, -1, pos=(20,45), size=(40,-1))
-                wx.StaticText(self, -1, 'Vorname.', pos=(20,70))
-                self.name = wx.TextCtrl(self, -1, size=(150,-1), pos=(20,95))
-                wx.StaticText(self, -1, 'Nachname', pos=(20,120))
-                self.famname = wx.TextCtrl(self, -1, size=(150,-1), pos=(20,145))
-                wx.StaticText(self, -1, 'Geburtsdatum', pos=(20,170))
-                self.date = wx.TextCtrl(self, -1, size=(150,-1), pos=(20,195))
-                wx.StaticText(self, -1, 'Klasse', pos=(20,220))
-                self.clas = wx.ComboBox(self, value="Klasse Auswählen", choices=self.DropDownList, size=(150,-1), pos=(20,245), style=wx.CB_SORT)
-                self.buttonsave = wx.Button(self, -1, label='Hinzufügen', pos=(650,290), size=(100,25))
-                self.buttondelete = wx.Button(self, -1, label='Löschen', pos=(760,290), size=(100,25))
-                self.buttonadd_class = wx.Button(self, -1, label='Klasse hinzufügen', pos=(650,320), size=(210,25))
-                self.buttonsave.Bind(wx.EVT_BUTTON , self.__save)
-                self.buttondelete.Bind(wx.EVT_BUTTON , self.__delete)
-                self.buttonadd_class.Bind(wx.EVT_BUTTON , self.__add_class)
+                self.nbrhead = wx.StaticText(self, -1, 'Nr.')
+                self.nbr = wx.TextCtrl(self, -1, size=(40,-1))
+                inbox.Add(self.nbrhead, flag=wx.TOP, border=10)
+                inbox.Add(self.nbr, flag=wx.TOP, border=8)
+                self.namehead = wx.StaticText(self, -1, 'Vorname.')
+                self.name = wx.TextCtrl(self, -1, size=(150,-1))
+                inbox.Add(self.namehead, flag=wx.TOP, border=10)
+                inbox.Add(self.name, flag=wx.TOP, border=8)
+                self.famnamehead = wx.StaticText(self, -1, 'Nachname')
+                self.famname = wx.TextCtrl(self, -1, size=(150,-1))
+                inbox.Add(self.famnamehead, flag=wx.TOP, border=10)
+                inbox.Add(self.famname, flag=wx.TOP, border=8)
+                self.datehead = wx.StaticText(self, -1, 'Geburtsdatum')
+                self.date = wx.TextCtrl(self, -1, size=(150,-1))
+                inbox.Add(self.datehead, flag=wx.TOP, border=10)
+                inbox.Add(self.date, flag=wx.TOP, border=8)
+                self.clashead = wx.StaticText(self, -1, 'Klasse')
+                self.clas = wx.ComboBox(self, value="Klasse Auswählen", choices=self.DropDownList, size=(150,-1), style=wx.CB_SORT)
+                inbox.Add(self.clashead, flag=wx.TOP, border=10)
+                inbox.Add(self.clas, flag=wx.TOP, border=8)
 
                 # create the list
                 
-                self.dl = wx.ListCtrl(self, -1, size=(640,250), pos=(220,20), style=wx.LC_REPORT | wx.BORDER_SUNKEN)
+                self.dl = wx.ListCtrl(self, -1, style=wx.LC_REPORT | wx.BORDER_SUNKEN)
                 self.dl.InsertColumn(0, 'Nr')
                 self.dl.InsertColumn(1, 'Nachname')
                 self.dl.InsertColumn(2, 'Vorname')
@@ -50,7 +60,30 @@ class DriverList(wx.Dialog):
                 self.dl.SetColumnWidth(1, 150)
                 self.dl.SetColumnWidth(2, 150)
                 self.dl.SetColumnWidth(3, 100)
-                self.dl.SetColumnWidth(4, 100)
+                self.dl.SetColumnWidth(4, 150)
+              
+
+                # buttons
+                self.buttonsave = wx.Button(self, -1, label='Hinzufügen', size=(100,25))
+                buttonbox1.Add(self.buttonsave)
+                self.buttondelete = wx.Button(self, -1, label='Löschen', size=(100,25))
+                buttonbox1.Add(self.buttondelete)
+                self.buttonadd_class = wx.Button(self, -1, label='Klasse hinzufügen', size=(200,25))
+                inbox.Add((-1,75))
+                inbox.Add(buttonbox1)
+                inbox.Add(self.buttonadd_class)
+                hbox.Add(inbox, flag=wx.BOTTOM, border=25)
+                hbox.Add(self.dl, flag=wx.LEFT|wx.GROW|wx.ALL, border=25)  
+
+                # Events
+                self.buttonsave.Bind(wx.EVT_BUTTON , self.__save)
+                self.buttondelete.Bind(wx.EVT_BUTTON , self.__delete)
+                self.buttonadd_class.Bind(wx.EVT_BUTTON , self.__add_class)
+                
+
+                self.SetSizer(hbox)
+                self.SetAutoLayout(True)
+
                 
                 # add lines from database
                 self.__update_list()
